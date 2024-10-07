4a99222ae1a54238fec6966e0c9c135849347509
leerlandais <leerlandais@gmail.com>
2024-10-07 17:59:19 +0200

    added 404

--- Template-BS-Personal/controller/publicController.php
+++ Template-BS-Personal/controller/publicController.php
@@ -1,3 +1,16 @@
 <?php
 
-echo $twig->render("/public/public.home.html.twig");
+
+$route = $_GET['route'] ?? 'home';
+    switch ($route) {
+        case 'home':
+        echo $twig->render("/public/public.home.html.twig");
+            break;
+        case 'about':
+            echo $twig->render("/public/public.about.html.twig");
+            break;
+        default:
+            echo $twig->render("err404.html.twig");
+    }
+
+
